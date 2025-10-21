2f77fc43df0f78fae6344c408f82076563e21665
Oussama Moutafatin <oussama@moutafatin.com>
2025-10-21 23:51:36 +0100

    nvim: center mini.pick window

--- .config/nvim/lua/plugins/mini-nvim.lua
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -9,7 +9,21 @@ require("mini.cursorword").setup()
 require("mini.indentscope").setup()
 require("mini.notify").setup()
 require("mini.statusline").setup()
-require("mini.pick").setup()
+require("mini.pick").setup({
+	window = {
+		config = function()
+			local height = math.floor(0.618 * vim.o.lines)
+			local width = math.floor(0.618 * vim.o.columns)
+			return {
+				anchor = "NW",
+				height = height,
+				width = width,
+				row = math.floor(0.5 * (vim.o.lines - height)),
+				col = math.floor(0.5 * (vim.o.columns - width)),
+			}
+		end,
+	},
+})
 require("mini.extra").setup()
 -- require("mini.animate").setup()
 require("mini.move").setup({
